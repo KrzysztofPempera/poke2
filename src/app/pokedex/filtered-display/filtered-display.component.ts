355b4bf34a3d3e9eb652fc39433c02a489e20519
Krzysztof Pempera <krzpem@st.amu.edu.pl>
2020-04-16 13:13:08 +0200

    Added filtered list pagination

--- src/app/pokedex/filtered-display/filtered-display.component.ts
+++ src/app/pokedex/filtered-display/filtered-display.component.ts
@@ -11,14 +11,38 @@ import { Pokemon, Pokemon2 } from '../pokemon.models';
 export class FilteredDisplayComponent implements OnInit {
 
   filteredData: Pokemon[];
-  pokemon: Pokemon2;
+  pokemonList: Pokemon2[];
+  totalPages: number;
+  pageNumber = 1;
+  pageLimit = 40;
 
   constructor(private pokeService: PokedexService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.filteredData = this.route.snapshot.data.type;
-    this.pokemon = this.filteredData[0].pokemon;
-    console.log(this.pokemon);
+    this.totalPages = this.getTotalPages();
+    this.pokemonList = this.getPage(this.pageNumber);
   }
 
+  getTotalPages(): number {
+    return Math.ceil(this.filteredData.length / 40);
+  }
+  getPage(pageNumber: number): Pokemon2[] {
+    let index = ((pageNumber - 1) * this.pageLimit);
+    if (pageNumber !== this.totalPages){
+      const testArray: Pokemon2[] = new Array(this.pageLimit);
+      for (let i = 0; i < this.pageLimit; i++, index++) {
+        testArray[i] = this.filteredData[index].pokemon;
+      }
+      return testArray;
+    }
+    else if (pageNumber === this.totalPages) {
+      const pageLimit = this.filteredData.length % this.pageLimit;
+      const testArray: Pokemon2[] = new Array(pageLimit);
+      for (let i = 0; i < pageLimit; i++, index++) {
+        testArray[i] = this.filteredData[index].pokemon;
+      }
+      return testArray;
+    }
+  }
 }
